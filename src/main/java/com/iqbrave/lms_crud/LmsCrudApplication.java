cb5ba2cf775df3fbcbd5f7b165ba321cfc2056d2
unknown <dmahesh9810@gmail.com>
2025-09-09 18:32:42 +0530

    Initial commit

---
+++ src/main/java/com/iqbrave/lms_crud/LmsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.iqbrave.lms_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iqbrave/lms_crud/LmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbrave.lms_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
